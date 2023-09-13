683af889e93dd4e1ff90aac1e46e1230b8b32610
mucahit <mucahitakdeniz28@gmail.com>
2023-09-13 21:51:20 +0300

    oop

---
+++ oop.js
@@ -0,0 +1,43 @@
+"use strict";
+
+//? **** OBJECT ****
+
+// const exepleObject={
+//     propertyName:"value",
+//     methodName:()=>{
+//         return "This is Method"
+//     }
+// }
+
+// console.log(exepleObject.propertyName);
+
+// let a =exepleObject.methodName()
+// console.log(a);
+/*--------------------------------------
+
+//? "THİS" KEYWORD"
+
+const car = {
+  brand: "Ford",
+  model: "Mustang",
+  year: 1967,
+  isAutoGear: true,
+  colors: ["white", "red"],
+  details: {
+    color: "red",
+    color2: "white",
+  },
+  startEngine: function () {
+    return "Motor çalıiştı";
+  },
+  getDetails: function () {
+    return this;
+  },
+  getDetails1: function () {
+    return this.year + " Model "+ this.brand + " " + this.model +" " ;
+  },
+};
+console.log(car.getDetails());
+console.log(car.getDetails1());
+/*--------------------------------------*/
+
