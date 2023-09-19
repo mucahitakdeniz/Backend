d010b6b90e32b84e676f6a84e6126e41a3a4de30
Mücahit Akdeniz <mucahitakdeniz28@gmail.comgit config --global user.email mucahitakdeniz28@gmail.com>
2023-09-19 18:40:19 +0300

    tw2

--- tw/index.js
+++ tw/index.js
@@ -1,3 +1,37 @@
-let a = [10, 20, 30];
-a[10] = 100;
-console.log(a.length);
+class Person {
+  constructor(name) {
+    this.name = name;
+  }
+  sayName() {
+    return this.name;
+  }
+}
+class BankAccount extends Person {
+  constructor(name, balance) {
+    super(name) ;
+    this.balance = balance;
+  }
+  deposit(money) {
+    if (money > 0) {
+      this.balance += money;
+    } else {
+      console.log("Eksik veya Hatalı miktar girdiniz");
+    }
+  }
+  withdraw(money) {
+    if (money > 0 && this.balance >= money) {
+      console.log((this.balance -= money));
+    } else if (money >= this.balance) {
+      console.log("Bu işlem icin yeterli Bakiyeniz Bulunmamaktadır ");
+    } else {
+      console.log("Eksik veya Hatalı miktar girdiniz");
+    }
+  }
+  balance() {
+    return `Bakiyeniz : ${this.balance}`;
+  }
+}
+
+const garantBank = new BankAccount("Mucahit", 2000);
+
+console.log(garantBank.sayName());
