ea4125a6fac1931cf40732a5572b5c493b155647
mucahit <mucahitakdeniz28@gmail.com>
2023-09-28 22:36:37 +0300

    builtins

--- 10-Routers_Errors/builtins.js
+++ 10-Routers_Errors/builtins.js
@@ -11,8 +11,25 @@ const PORT = process.env.PORT || 8000;
 
 /* ------------------------------------------------------- */
 
+/* Data Receiving */
+//? Read json data
+app.use(express.json())
+//? Read text data
+app.use(express.text())
+//? Read form data
+// extended must be define. (if true, it supported nested form-data.)
+app.use(express.urlencoded({ extended: true })) 
 
+//? receive sent-data with req.body:
+app.all('/', (req, res) => {
+    res.send({
+        received: true,
+        body: req.body
+    })
+})
 
+/* Call staticFiles from real-path */
+app.use('/img', express.static('./public/images'))
 
 /* ------------------------------------------------------- */
 
