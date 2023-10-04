82c06f88e3c4436e8deb3acadbb500314af7611e
mucahit <mucahitakdeniz28@gmail.com>
2023-10-04 21:01:01 +0300

    dbConnection

--- 13-BlogAPI_Mongoose/src/dbConnection.js
+++ 13-BlogAPI_Mongoose/src/dbConnection.js
@@ -1,4 +1,11 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     EXPRESSJS - BLOG Project with Mongoose
 ------------------------------------------------------- */
+
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(process.env.MONGODB || "mongodb://localhost:27017/")
+  .then(() => console.log(" ** DB Connected **"))
+  .catch(() => console.log(" ** DB Not Connected**"), err);
--- 13-BlogAPI_Mongoose/src/errorHandler.js
+++ 13-BlogAPI_Mongoose/src/errorHandler.js
@@ -1,4 +1,15 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     EXPRESSJS - BLOG Project with Mongoose
 ------------------------------------------------------- */
+
+module.exports = (err, req, res, next) => {
+  const errorStatusCode = res.errorStatusCode ?? 500;
+
+  res.status().send({
+    error:true,
+    message:err.message,
+    cause:err.cause,
+    body:req.body
+  });
+};
