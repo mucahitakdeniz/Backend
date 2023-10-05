44a34a71acd6f3cfe3eb95d07851c30bda71bec8
mucahit <mucahitakdeniz28@gmail.com>
2023-10-05 20:32:20 +0300

    CategoryModel

--- 13-BlogAPI_Mongoose/src/dbConnection.js
+++ 13-BlogAPI_Mongoose/src/dbConnection.js
@@ -1,11 +1,13 @@
-"use strict";
+"use strict"
 /* -------------------------------------------------------
     EXPRESSJS - BLOG Project with Mongoose
 ------------------------------------------------------- */
 
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
-mongoose
-  .connect(process.env.MONGODB || "mongodb://localhost:27017/")
-  .then(() => console.log(" ** DB Connected **"))
-  .catch(() => console.log(" ** DB Not Connected**"), err);
+// const MONGODB = process.env.MONGODB || 'mongodb://localhost:27017/blogAPI'
+// mongoose.connect(MONGODB)
+
+mongoose.connect(process.env.MONGODB)
+    .then(() => console.log(' * DB Connected * '))
+    .catch((err) => console.log(' * DB Not Connected * ', err))
