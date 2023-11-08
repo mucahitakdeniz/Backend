de10f5323b038b678e75c673ad1a6a366bcd72c7
mucahit <mucahitakdeniz28@gmail.com>
2023-11-08 20:07:14 +0300

    up

---
+++ 29_TodoAPP_EJS/app.js
@@ -0,0 +1,38 @@
+"use strict";
+/* -------------------------------------------------------
+    EXPRESSJS - TODO Project with Sequelize
+------------------------------------------------------- */
+
+const express = require("express");
+const app = express();
+
+require("dotenv").config();
+const PORT = process.env.PORT || 8000;
+//---------------------------------------------------------------/
+//Template
+
+//console.log(app);
+
+app.set('view engine','ejs')
+
+app.set('views','public')
+/* ------------------------------------------------------- */
+// Accept json data & convert to object:
+app.use(express.json())
+
+// Router:
+app.all('/',(req,res)=>{
+    res.render('index')
+})
+app.use('/view',require('./app/routes/todoTemplate'))
+app.use('/api',require('./app/routes/todo'))
+
+// DatabaseConnection:
+const { dbConnection } = require('./app/dbConnection')
+dbConnection() // sequelize.sync() must run after model defines.
+
+// errorHandler (Catch Errors):
+app.use(require('./app/errorHandler'))
+
+/* ------------------------------------------------------- */
+app.listen(PORT, () => console.log("Running: http://127.0.0.1:" + PORT));
